49e19ba79fccb3c14920ed7ac8aa676bc22fd98c
ben <boneill@rnet-tech.com>
2021-08-18 15:36:43 -0400

    updates to new api

--- ref/vnv_MINIAMR.c
+++ ref/vnv_MINIAMR.c
@@ -1,20 +1,4 @@
-///7070386492871253011
-/// This file was automatically generated using the VnV-Matcher executable. 
-/// The matcher allows for automatic registration of all VnV plugins and injection 
-/// points. Building the matcher requires Clang. If Clang is not available on this machine,
-/// Registration code should be written manually. 
-/// 
+#include "VnV.h"
 
-//PACKAGENAME: MINIAMR
-
-#include "VnV.h" 
-const char* getFullRegistrationJson_MINIAMR(){
-	 return "{\"Communicator\":{\"docs\":\"\",\"name\":\"mpi\",\"package\":\"VnV\"},\"Introduction\":\"\"}";}
-
-INJECTION_REGISTRATION(MINIAMR){
-	VnV_Declare_Communicator("MINIAMR","VnV","mpi");
-	REGISTER_FULL_JSON(MINIAMR, getFullRegistrationJson_MINIAMR);
-};
-
-
+INJECTION_REGISTRATION(MINIAMR){}
 
