46b2688ae810118ae36749bd3708e3e0c1d6710b
ben <boneill@rnet-tech.com>
2021-09-10 15:51:27 -0400

    added input file

--- ref/vnv_MINIAMR.c
+++ ref/vnv_MINIAMR.c
@@ -1,4 +1,24 @@
-#include "VnV.h"
+///MINIAMR:13607191176472651629
+/// This file was automatically generated using the VnV-Matcher executable. 
+/// The matcher allows for automatic registration of all VnV plugins and injection 
+/// points. Building the matcher requires Clang. If Clang is not available on this machine,
+/// Registration code should be written manually. 
+/// 
 
-INJECTION_REGISTRATION(MINIAMR){}
+//PACKAGENAME: MINIAMR
+
+#include "VnV.h" 
+#include "version/version.h"
+
+
+VNVVERSIONINFOCALLBACK(MINIAMR)
+
+const char* getFullRegistrationJson_MINIAMR(){
+	 return "{}";}
+
+INJECTION_REGISTRATION(MINIAMR){
+	REGISTER_FULL_JSON(MINIAMR, getFullRegistrationJson_MINIAMR);
+};
+
+
 
